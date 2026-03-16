9588d82cee026b99ff839ecea89e4e7d86fb94fd
longyou <yuhaonan@outlook.com>
2026-03-16 11:25:43 +0800

    完成搜索页面

--- meikoya/features/my/hvigorfile.ts
+++ meikoya/features/my/hvigorfile.ts
@@ -1,6 +1,7 @@
 import { hspTasks } from '@ohos/hvigor-ohos-plugin';
+import { hspPlugin } from "@hadss/hmrouter-plugin";
 
 export default {
   system: hspTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [hspPlugin()]         /* Custom plugin to extend the functionality of Hvigor. */
 }
